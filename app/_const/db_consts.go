df48729fb7e2ed51995a7d60436ad634747eaab8
edorenta <edorenta@gmail.com>
2018-11-07 18:54:25 +0100

    alpha deployable

--- app/_const/db_consts.go
+++ app/_const/db_consts.go
@@ -5,7 +5,7 @@ const (
 	DB_PORT uint16	= 5432
 	DB_USER			= "postgres"
 	// DB_PWD 	= "" //not storing any password in the code, the admin will be asked for psql pwd at start time
-	DB_NAME  		= "go_mt4" //all the further DB info is retrieved in db.go
+	DB_NAME  		= "webserver" //all the further DB info is retrieved in db.go
 	Q_INSERT		= 1
 	Q_MODIFY		= 2
 	Q_DELETE		= 3
