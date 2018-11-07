275e58eb1a1ea2defd08fe591de9bc9686930e76
edorenta <edorenta@gmail.com>
2018-11-07 20:37:24 +0100

    alpha deployable

--- app/_const/webserver_consts.go
+++ app/_const/webserver_consts.go
@@ -1,8 +1,8 @@
 package _const
 
 const (
-	APP_PORT uint16 		= 49152
-	DOMAIN string			= "http://paul-r.com"
+	APP_PORT uint16 		= 8080
+	DOMAIN string			= "http://localhost"
 	HTTPS bool 				= false
 	SSL_KEY_PATH string 	= "../ssl/http.key"
 	SSL_CRT_PATH string 	= "../ssl/http.pem"
