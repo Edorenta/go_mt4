287cc2e908dc0d0595de652fd3b25a8a2dab2073
Paul de Renty <edorenta@gmail.com>
2018-12-06 16:35:22 +1300

    Update webserver_consts.go

--- app/_const/webserver_consts.go
+++ app/_const/webserver_consts.go
@@ -1,9 +1,11 @@
 package _const
 
 const (
-	APP_PORT uint16 		= 8080
-	DOMAIN string			= "localhost:8080"
-	HTTPS bool 				= false
+	//APP_PORT uint16 		= 8080
+	//DOMAIN string			= "localhost:8080"
+	APP_PORT uint16 		= 80 //prod
+	DOMAIN string			= ":80" //prod
+	HTTPS bool 			= false
 	SSL_KEY_PATH string 	= "../ssl/http.key"
 	SSL_CRT_PATH string 	= "../ssl/http.pem"
 	FALLBACK string			= "/captcha?"
