f8d8a00d1fc3dea5e76bba6be64b5eb88a8b5edb
edorenta <edorenta@gmail.com>
2018-12-08 09:04:40 +1300

    *

--- app/_const/webserver_consts.go
+++ app/_const/webserver_consts.go
@@ -1,10 +1,10 @@
 package _const
 
 const (
-	//APP_PORT uint16 		= 8080
-	//DOMAIN string			= "localhost:8080"
-	APP_PORT uint16 		= 80 //prod
-	DOMAIN string			= ":80" //prod
+	APP_PORT uint16 		= 8080
+	DOMAIN string			= "localhost:8080"
+	// APP_PORT uint16 		= 80 //prod
+	// DOMAIN string			= ":80" //prod
 	HTTPS bool 			= false
 	SSL_KEY_PATH string 	= "../ssl/http.key"
 	SSL_CRT_PATH string 	= "../ssl/http.pem"
